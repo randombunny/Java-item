fe2b4971be8e38ab7be6823502b9b18267ef487b
randombunny <55337118+randombunny@users.noreply.github.com>
2020-10-15 20:33:29 +0800

    Update SecurityGuard.java

--- experiment4/src/two/SecurityGuard.java
+++ experiment4/src/two/SecurityGuard.java
@@ -33,4 +33,50 @@ public class SecurityGuard extends Staff{
     public void setDangerousAllowance(int dangerousAllowance) {
         this.dangerousAllowance = dangerousAllowance;
     }
+
+    @Override
+    public int getSalary() {
+        return super.getSalary() + this.getDangerousAllowance();
+    }
+}
+package two;
+
+import java.util.Date;
+
+public class SecurityGuard extends Staff{
+    private String skills;
+    private int dangerousAllowance;
+
+    public SecurityGuard(String skills, int dangerousAllowance,String name, String address, int age, boolean sex, int salary, Date dataHired) {
+        super(name, address, age, sex, salary, dataHired);
+        this.skills = skills;
+        this.dangerousAllowance = dangerousAllowance;
+    }
+
+    public SecurityGuard() {
+        this.skills = "";
+        this.dangerousAllowance = 0;
+
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public int getDangerousAllowance() {
+        return dangerousAllowance;
+    }
+
+    public void setDangerousAllowance(int dangerousAllowance) {
+        this.dangerousAllowance = dangerousAllowance;
+    }
+
+    @Override
+    public int getSalary() {
+        return super.getSalary() + this.getDangerousAllowance();
+    }
 }
